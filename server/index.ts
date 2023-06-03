27960a2eca591a4ca97e6c3504155f855383e51d
Ellie K <61605543+king-ellie@users.noreply.github.com>
2023-06-02 22:05:47 -0400

    Add eslint + prettier to repo (#3)
    * add eslint and prettier to dependencies + config files
    
    * format files w/ eslint and prettier

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,32 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use(express.json())
+app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use(express.json());
 
-app.get('/', (_req, res, _next) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
-})
+app.get("/", (_req, res, _next) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
-const init = async() => {
-    try {
-        // await db.sync()
-        const port = process.env.PORT || 3000
-        app.listen(port, () => console.log(`
+const init = async () => {
+  try {
+    // await db.sync()
+    const port = process.env.PORT || 3000;
+    app.listen(port, () =>
+      console.log(`
             Listening on port ${port}
 
             http://localhost:${port}/
-        `))
-    }
-    catch(error) {
-        console.log('SERVER INIT ERROR: ', error)
-    }
-}
-init()
+        `)
+    );
+  } catch (error) {
+    console.log("SERVER INIT ERROR: ", error);
+  }
+};
+init();
